849529aa925577effc9a27ee93a8c19d3ff780eb
xiejiangchu <1235116@tongji.edu.cn>
2016-10-29 15:00:27 +0800

    上传文件功能

--- WebRoot/js/controller/mainController.js
+++ WebRoot/js/controller/mainController.js
@@ -1,4 +1,4 @@
-vr.controller('mainController', function ($scope, $http, $state, $dictionary) {
+vr.controller('mainController', function ($rootScope, $scope, $http, $state, $stateParams, $dictionary) {
     $state.go('home');
     $scope.index = 'hot';
     $scope.category = $dictionary.category;
@@ -10,4 +10,14 @@ vr.controller('mainController', function ($scope, $http, $state, $dictionary) {
     $scope.typeChange = function (id) {
         $scope.select_type = id;
     }
+    $scope.state = 'home';
+    $rootScope.$on('$stateChangeStart',
+        function (event, toState, toParams, fromState, fromParams) {
+            if (toState.name == 'home' || toState.name == 'upload' || toState.name == 'profile') {
+                $scope.state = toState.name;
+            } else {
+                $scope.state = 'other';
+            }
+
+        });
 });
--- WebRoot/js/controller/uploadController.js
+++ WebRoot/js/controller/uploadController.js
@@ -1,3 +1,45 @@
 vr.controller('uploadController', function ($scope, $http, $state, $dictionary) {
+    $scope.PICTURES_MAX = 2;
+    $scope.pictures = [];
+    $scope.files = []
+    $scope.show_add = true;
+    $scope.picture_select = function ($ele) {
+        if ($ele.files) {
+            var add_file = $ele.files[0];
+            var allowExtention = ".jpg,.bmp,.gif,.png";
+            var extention = add_file.name.substring(add_file.name.lastIndexOf(".") + 1).toLowerCase();
+            if (allowExtention.indexOf(extention) > -1) {
+                $scope.pictures.push(window.URL.createObjectURL(add_file));
+                $scope.files.push(add_file);
+                $ele.files = null;
+                if ($scope.pictures.length >= $scope.PICTURES_MAX) {
+                    $scope.show_add = false;
+                }
+                $scope.$apply();
+            }
+        }
+    };
+    $scope.upload = function () {
+        var formdata = new FormData();
+        formdata.append('text', $scope.uploadData.remark);
+        for (var i = 0; i < $scope.files.length; i++) {
+            formdata.append('file[]', $scope.files[i]);
+        }
+        $http({
+            method: 'POST',
+            url: "/upload",
+            data: formdata,
+            headers: {'Content-Type': undefined},
+            transformRequest: angular.identity
+        }).success(function (response) {
+            //上传成功的操作
+            alert("uplaod success");
+        });
 
+    }
+
+    $scope.picture_click = function ($index) {
+        $scope.pictures.splice($index, 1);
+        $scope.files.splice($index, 1);
+    }
 });
