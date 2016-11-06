2165ca35d21282531c4da9166ddc271ff26d5034
lijingfeiniao <1294057732@qq.com>
2016-11-06 23:10:28 +0800

    上传功能代码提交，包括batis

--- WebRoot/js/controller/uploadController.js
+++ WebRoot/js/controller/uploadController.js
@@ -1,3 +1,21 @@
 vr.controller('uploadController', function ($scope, $http, $state, $dictionary) {
+    $scope.save = function() {
+        var fd = new FormData();
+        fd.append('file', $scope.myFile);
+        fd.append('desc',  $scope.desc);
+        $http({
+            method:'POST',
+            url:"image/upload",
+            data: fd,
+            headers: {'Content-Type':undefined},
+            transformRequest: angular.identity
+        })
+            .success( function ( response )
+            {
+                //上传成功的操作
+                alert("uplaod success"+$scope.desc+"--"+file);
+            });
 
-});
+    }
+})
+
